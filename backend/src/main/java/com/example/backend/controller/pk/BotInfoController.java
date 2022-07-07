0587cd430a9204ad27e2ce8f484e39e3fb6449f4
shihao ren <rsh15355756202@163.com>
2022-07-08 00:27:36 +0800

    项目骨架搭建好了

--- backend/src/main/java/com/example/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/example/backend/controller/pk/BotInfoController.java
@@ -9,17 +9,10 @@ import java.util.*;
 @RequestMapping("/pk")
 public class BotInfoController {
     @RequestMapping("/getbotinfo")
-    public List<Map<String, String>> getBotInfo(){
-        List<Map<String, String>> list = new LinkedList<>();
-        Map<String, String> map1 = new HashMap<>();
-        map1.put("rsh", "1500");
-        map1.put("jbb", "1800");
-
-        Map<String, String> map2 = new HashMap<>();
-        map2.put("tom", "1600");
-        map2.put("mike", "1300");
-        list.add(map1);
-        list.add(map2);
-        return list;
+    public Map<String, String> getBotInfo(){
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "tiger");
+        map.put("rating", "1900");
+        return map;
     }
 }
